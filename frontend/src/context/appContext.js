6ada06c0e6fc17aa7d90b373ca9ff1674efc1869
LexieLiu19 <xuliu0925@gmail.com>
2023-06-06 16:48:20 -0400

    add/remove user to/from localstorage in register function
    Signed-off-by: LexieLiu19 <xuliu0925@gmail.com>

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -9,16 +9,18 @@ import {
 } from './actions';
 
 import axios from 'axios';
-
-export const initialState = {
+const token = localStorage.getItem('token');
+const user = localStorage.getItem('user');
+const userLocation = localStorage.getItem('location');
+const initialState = {
   isLoading: false,
   showAlert: true,
   alertText: '',
   alertType: '',
-  user: null,
-  token: null,
-  userLocation: '',
-  jobLocation: '',
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || '',
+  jobLocation: userLocation || '',
 };
 
 const AppContext = React.createContext();
@@ -34,18 +36,28 @@ const AppProvider = ({ children }) => {
       dispatch({ type: CLEAR_ALERT });
     }, 3000);
   };
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+    localStorage.setItem('location', location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('location');
+  };
 
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
       const response = await axios.post('/api/v1/auth/register', currentUser);
-      console.log(response);
 
       const { user, token, location } = response.data;
+
+      addUserToLocalStorage({ user, token, location });
       dispatch({ type: REGISTER_USER_SUCCESS, payload: { user, token, location } });
     } catch (error) {
-      console.log(error.response);
-
       dispatch({ type: REGISTER_USER_ERROR, payload: { msg: error.response.data.msg } });
     }
 
@@ -63,4 +75,4 @@ export const useAppContext = () => {
   return useContext(AppContext);
 };
 
-export { AppProvider };
+export { AppProvider, initialState };
