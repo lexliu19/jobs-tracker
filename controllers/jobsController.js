cdd49fb57bf336a1d0ee3f652904ae061ca54884
LexieLiu19 <xuliu0925@gmail.com>
2023-08-17 16:01:59 -0400

    add permission check to updateJob
    Signed-off-by: LexieLiu19 <xuliu0925@gmail.com>

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -24,7 +25,24 @@ const deleteJobs = async (req, res) => {
   res.send('delete Job');
 };
 const updateJob = async (req, res) => {
-  res.send('update Job');
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    throw new BadRequestError('Please provide all values');
+  }
+  const job = await Job.findOne({ _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 const showStats = async (req, res) => {
   res.send('show stats Job');
