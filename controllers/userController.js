30d6a3f6b4abbde0b88a1cfe1de85cd4920d1e43
LexieLiu19 <xuliu0925@gmail.com>
2024-08-29 16:38:31 -0400

    update avatar to profile
    Signed-off-by: LexieLiu19 <xuliu0925@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,38 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
+import cloudinary from 'cloudinary';
+import { promises as fs } from 'fs';
 
-const getCurrentUser = async (req, res) => {
+export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
   const userWithoutPassword = user.toJSON(); //`toJSON` method defined in the User model to delete password
   res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
-const getApplicationStats = async (req, res) => {
+export const getApplicationStats = async (req, res) => {
   const users = await User.countDocuments();
   const jobs = await Job.countDocuments();
 
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 
-const updateUser = async (req, res) => {
-  console.log(req.file);
-  await User.findByIdAndUpdate(req.user.userId, req.body);
-  res.status(StatusCodes.OK).json({ message: 'User updated' });
+export const updateUser = async (req, res) => {
+  const newUser = { ...req.body };
+  delete newUser.password;
+
+  //if user is updating avatar image:
+  if (req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+  //updatedUser is the user data before updating
+  //remove the old avatar
+  if (req.file && updatedUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
+  }
+  res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
-export { getCurrentUser, updateUser, getApplicationStats };
