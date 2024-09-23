7f1efdf4e26b55e66b74492e4e3b182438e34317
LexieLiu19 <xuliu0925@gmail.com>
2024-09-23 16:50:02 -0400

    setup local build

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 //user route:
 app.use('/api/v1/users', authenticateUser, userRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
+
 //not found middleware"
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
